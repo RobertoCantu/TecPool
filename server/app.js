ad672fe546a77fcfdf053e1cd48b22860f8d568c
Gustavo Benitez <benitezlgustavo@gmail.com>
2022-04-23 15:41:53 -0500

    Mail Test

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from 'cors';
 import dotenv from "dotenv";
 import { whatsapp } from "./utils/twilio.js";
+import { transporter } from './utils/nodemailer.js';
 
 dotenv.config();
 
@@ -23,9 +24,24 @@ const hostname = 'localhost';
 
 const app = express();
 
-// app.get("/twilio", ()=>{
-//     whatsapp('8134444000', 'Hello World');
-// })
+app.post("/twilio", ()=>{
+    whatsapp('8134444000', 'Hello World');
+})
+
+app.post('/send-email', ()=>{
+    transporter.sendMail({
+        from: '"TecPools" <tecpools@outlook.com>',
+        to: 'ntw85363@jiooq.com',
+        subject: "Bienvenido a TecPools",
+        html: "<h1>Bienvenido a TecPools</h1><p>Tu cuenta fue creada exitosamente.</p><p>Inicia sesión en tecpools.tec.mx.<p>",
+    }, (err, info) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(info.response)
+    });  
+})
 
 app.use(express.json()); // to accept json data
 app.use(cors({
